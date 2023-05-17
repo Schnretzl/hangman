d0fd75bb284501827ef06532a305e55fc5669470
Wes Hendley <wrhendley@gmail.com>
2023-05-17 11:49:50 -0500

    Update class name to follow Ruby conventions.  Switch to #chars() method.  Make @guess_letter_array be assigned to itself in make_guess method

--- word.rb
+++ word.rb
@@ -1,19 +1,18 @@
-class hangman_word
-    attr_accessor :word, 
+class HangmanWord
+  attr_accessor :word
 
-    def initialize(word)
-        @word = word
-        @guesses = 0
-        @answer_letter_array = word.split
-        @guess_letter_array = Array.new(@word.length, '_')
+  def initialize(word)
+    @word = word
+    @guesses = 0
+    @answer_letter_array = word.split('')
+    @guess_letter_array = Array.new(@word.length, '_')
+  end
 
+  def make_guess(letter)
+    if @word.include?(letter)
+      @guess_letter_array = @guess_letter_array.zip(@answer_letter_array).map { |guess_letter, word_letter| word_letter == letter ? letter : guess_letter }
+    else
+      @guesses += 1
     end
-
-    def make_guess(letter)
-        if !@word.include?(letter)
-            @guesses += 1
-        else
-            @guess_letter_array.zip(@answer_letter_array).map { |guess_letter, word_letter| word_letter == letter ? letter : guess_letter }
-        end
-    end
+  end
 end
