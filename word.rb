859de589cd6389d4d0087846ab653976c956c195
Wes Hendley <wrhendley@gmail.com>
2023-05-17 11:38:02 -0500

    Add make_guess method

--- word.rb
+++ word.rb
@@ -4,6 +4,8 @@ class hangman_word
     def initialize(word)
         @word = word
         @guesses = 0
+        @answer_letter_array = word.split
+        @guess_letter_array = Array.new(@word.length, '_')
 
     end
 
@@ -11,7 +13,7 @@ class hangman_word
         if !@word.include?(letter)
             @guesses += 1
         else
-            self.each(l)
+            @guess_letter_array.zip(@answer_letter_array).map { |guess_letter, word_letter| word_letter == letter ? letter : guess_letter }
         end
     end
 end
